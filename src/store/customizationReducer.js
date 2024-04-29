71e4321b80ea6204df69567b76e00d54a85a3c03
priyansh <upadhyayparth080@gmail.com>
2024-04-29 10:08:27 +0530

    code upload
    code upload

--- src/store/customizationReducer.js
+++ src/store/customizationReducer.js
@@ -5,7 +5,7 @@ import config from "config";
 import * as actionTypes from "./actions";
 
 export const initialState = {
-  isOpen: ["sample-page"], // for active default menu
+  isOpen: [""], // for active default menu
   defaultId: "default",
   fontFamily: config.fontFamily,
   borderRadius: config.borderRadius,
